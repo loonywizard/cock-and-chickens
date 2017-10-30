a8187ae152e3e508fc2126e7362a56084b8b41c2
Vladimir Nikitin <vladimirsuperwizard@gmail.com>
2017-10-30 22:06:24 +0300

    Remove old configs folder

--- src/configs/player.js
+++
@@ -1,9 +0,0 @@
-const playerConfig = {
-  position: { x: 150, y: 30 },
-  size: { x: 40, y: 40 },
-  maxSpeed: 0.25,
-  textureUrl: 'images/p2.png',
-  radius: 20,
-};
-
-export default playerConfig;
