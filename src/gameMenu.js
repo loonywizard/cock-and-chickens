7b9d3cd1cec883b51dfe7d008c4fd5cbdb5873cb
Vladimir Nikitin <vladimirsuperwizard@gmail.com>
2017-11-30 16:56:44 +0300

    Style game and gameover menus :nail_care:

--- src/gameMenu.js
+++ src/gameMenu.js
@@ -8,9 +8,13 @@ export default function GameMenu(store) {
   this.render = function () {
     const div = document.createElement('div');
     const playButton = document.createElement('div');
+
+    div.classList.add('game-menu');
+    playButton.classList.add('button');
     playButton.addEventListener('click', handlePlayButtonClick);
     playButton.innerHTML = 'Play';
     div.appendChild(playButton);
+
     return div;
   };
 }
--- src/gameOverMenu.js
+++ src/gameOverMenu.js
@@ -11,6 +11,7 @@ export default function GameOverMenu(store) {
 
   this.render = function (score) {
     const div = document.createElement('div');
+    div.classList.add('game-over-menu');
 
     const text = document.createElement('div');
     text.innerHTML = `Game over with score ${score}`;
@@ -19,11 +20,13 @@ export default function GameOverMenu(store) {
     const playButton = document.createElement('div');
     playButton.addEventListener('click', handlePlayButtonClick);
     playButton.innerHTML = 'Play';
+    playButton.classList.add('button');
     div.appendChild(playButton);
 
     const returnToMenuButton = document.createElement('div');
     returnToMenuButton.addEventListener('click', handleReturnToMenuButtonClick);
     returnToMenuButton.innerHTML = 'Return to menu';
+    returnToMenuButton.classList.add('button');
     div.appendChild(returnToMenuButton);
 
     return div;
